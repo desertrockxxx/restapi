471f73a73843af0b6c9155eeb72b48a6992c5359
Desertrockxxx <s.karateke@gmx.de>
2018-03-20 13:42:27 +0100

    Category erweitern um CRUD

---
+++ category/create.php
@@ -0,0 +1,36 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// get database connection
+include_once '../config/database.php';
+
+// instantiate category object 
+include_once '../objects/category.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$category = new Category($db);
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+// set category properly values
+$category->id = $data->id;
+$category->name = $data->name;
+$category->description = $data->description;
+$category->created = date('Y-m-d:i:s');
+
+// create the category
+if($category->create()){
+	echo '{';
+		echo '"message": "Category was created."';
+	echo '}';
+
+}
+?>
--- objects/category.php
+++ objects/category.php
@@ -15,21 +15,38 @@ class Category{
 		$this->conn = $db;
 	}
 
-	// used by select drop-down list
-	public function readAll(){
+	// create category
+	function create(){
 
-		// select all data
-		$query = 	"SELECT
-						id, name, description
-					FROM
-						" . $this->table_name ."
-					ORDER BY
-						name";
-
+		// query to insert record
+		$query = "INSERT INTO
+					" . $this->table_name . "
+				SET
+					id=:id,
+					name=:name,  
+					description=:description,
+					created=:created";
+		// prepare query
 		$stmt = $this->conn->prepare($query);
-		$stmt->execute();
 
-		return $stmt;
+		// sanitize
+		$this->id = htmlspecialchars(strip_tags($this->id));
+		$this->name = htmlspecialchars(strip_tags($this->name));
+		$this->description = htmlspecialchars(strip_tags($this->description));
+		$this->created = htmlspecialchars(strip_tags($this->created));
+
+		// bind values
+		$stmt->bindParam(":id", $this->id);
+		$stmt->bindParam(":name", $this->name);
+		$stmt->bindParam(":description", $this->description);
+		$stmt->bindParam(":created", $this->created);
+
+		// execute query
+		if($stmt->execute()){
+			return true;
+		}
+
+		return false;
 	}
 
 	// used by select drop-down list
@@ -50,6 +67,85 @@ class Category{
 
 	}
 
+	// used by select drop-down list
+	public function readAll(){
+
+		// select all data
+		$query = 	"SELECT
+						id, name, description
+					FROM
+						" . $this->table_name ."
+					ORDER BY
+						name";
+
+		$stmt = $this->conn->prepare($query);
+		$stmt->execute();
+
+		return $stmt;
+	}
+
+	// update the category 
+	function update(){
+
+		// update query
+		$query = "UPDATE
+					" . $this->table_name . "
+				SET
+					name = :name,
+					description = : description,
+					created = : created
+				WHERE
+					id = :id";
+
+		// prepare query statement
+		$stmt = $this->conn->prepare($query);
+
+		// sanitize
+		$this->name = htmlspecialchars(strip_tags($this->name));
+		$this->description = htmlspecialchars(strip_tags($this->description));
+		$this->created = htmlspecialchars(strip_tags($this->created));
+		$this->id = htmlspecialchars(strip_tags($this->id));
+
+		// bind new values
+		$stmt->bindParam(':name', $this->name);
+		$stmt->bindParam(':description', $this->description);
+		$stmt->bindParam(':created', $this->created);
+		$stmt->bindParam(':id', $this->id);
+
+		// execute the query 
+		if($stmt->execute()){
+			return true;
+		}
+
+		return false;
+	} 
+
+	// delete the category
+	function delete(){
+
+		// delete query
+		$query = "DELETE FROM " 
+					. $this->table_name . 
+				"WHERE id = ?";
+
+		// prepare query
+		$stmt = $this->conn->prepare($query);
+
+		// sanitize
+		$this->id = htmlspecialchars(strip_tags($this->id));
+
+		// bind id of record to delete 
+		$stmt->bindParam(1, $this->id);
+
+		// execute query
+		if($stmt->execute()){
+			return true;
+		}
+
+		return false;
+	}
+
+	
 
 }
 
